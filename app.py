33793edd10201fe9fe750f926cfec4f4efca63e8
Maxim Matyunin <mksmtn@tutanota.com>
2020-03-15 20:32:02 +0400

    Turn off debug mode

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class MainHandler(tornado.web.RequestHandler):
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
-  ], debug=True)
+  ])
 
 
 if __name__ == '__main__':
